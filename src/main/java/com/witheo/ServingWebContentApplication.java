f9307e01f6a35910c94d3e91f92a196f3918c3e4
witheo <theosw@gmail.com>
2021-06-20 10:33:29 -0400

    remove target

--- src/main/java/com/witheo/ServingWebContentApplication.java
+++ src/main/java/com/witheo/ServingWebContentApplication.java
@@ -3,12 +3,15 @@ package com.witheo;
 import com.witheo.marker.Markers;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.Marker;
+import org.slf4j.helpers.BasicMarker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ServingWebContentApplication {
     private static final Logger LOGGER = LoggerFactory.getLogger(ServingWebContentApplication.class);
+
 
     public static void main(String[] args) {
         SpringApplication.run(ServingWebContentApplication.class, args);
